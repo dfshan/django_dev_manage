8902932a8055a8b616efb547902277e99069bae3
shandf <shandanf@gmail.com>
2013-05-08 11:02:45 +0800

    update the template and enable device app

--- django_dev_manage/urls.py
+++ django_dev_manage/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,11 +11,14 @@ urlpatterns = patterns('',
     # url(r'^django_dev_manage/', include('django_dev_manage.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url( r'^index/', direct_to_template, {
         'template':'index.html'
+        }),
+    url( r'^login/', direct_to_template, {
+        'template':'login.html'
         }),
 )
