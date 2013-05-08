bb20dd89f9c8e3383cb3f0a65bc126c5a9596b9f
shandf <shandanf@gmail.com>
2013-05-08 21:23:14 +0800

    add person info

--- django_dev_manage/views.py
+++ django_dev_manage/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login, logout
@@ -11,8 +12,11 @@ def change_user( request ):
     psw = request.POST[ 'password' ]
     re_psw = request.POST[ 're_paw' ]
     email = request.POST[ 'email' ]
+    error = ''
     if ( len(psw) > 0  ) and ( cmp( psw, re_psw ) == 0 ):
         user.set_password( psw )
+    if cmp( psw, re_psw ) != 0:
+        error = u'两次密码输入不一致'
     if len(email) > 0:
         user.email = email
     user.save()
@@ -20,5 +24,6 @@ def change_user( request ):
     return render_to_response( 'person.html', {
         'username':user.username,
         'email':user.email,
-        'user':request.user
+        'user':request.user,
+        'error':error
         }, context_instance=RequestContext(request) );
