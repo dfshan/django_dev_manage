f076e6662377b9938290611baebe8e946fd173d5
shandf <shandanf@gmail.com>
2013-06-09 18:01:01 +0800

    displayed order time starts from now

--- device/views.py
+++ device/views.py
@@ -11,6 +11,16 @@ from django_dev_manage.views import check_user
 from datetime import datetime
 import datetime as dt
 
+def get_ord_time_from_now( dev ):
+    ord_time = OrderTime.objects.filter( dev=dev )
+    ord_time_from_now = []
+    for time in ord_time:
+        if time.end_time > datetime.now():
+            time.start_time = time.start_time.strftime( '%Y-%m-%d %H:%M:%S' )
+            time.end_time = time.end_time.strftime( '%Y-%m-%d %H:%M:%S' )
+            ord_time_from_now.append( time )
+    return ord_time_from_now
+            
 @csrf_protect
 def get_device(request):
     check = check_user( request )
@@ -29,7 +39,7 @@ def ord_dev_page(request):
         return check
     if 'id' in request.GET:
         dev = Device.objects.get( id = request.GET['id'] )
-        ord_time = OrderTime.objects.filter( dev = dev )
+        ord_time = get_ord_time_from_now( dev )
 
         return render_to_response( 'ord_dev.html', {
                     'dev': dev,
@@ -88,11 +98,7 @@ def device_info(request):
     ord_time = None
     if 'id' in request.GET:
         dev = Device.objects.get( id = request.GET['id'] )
-        ord_time = OrderTime.objects.filter( dev=dev )
-
-        for time in ord_time:
-            time.start_time = time.start_time.strftime( '%Y-%m-%d %H:%M:%S' )
-            time.end_time = time.end_time.strftime( '%Y-%m-%d %H:%M:%S' )
+        ord_time = get_ord_time_from_now(dev)
 
         dev.buy_date = dev.buy_date.strftime( '%Y-%m-%d' )
 
